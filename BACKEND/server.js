e5e6ae85d8af3c8119e64a3210a4a0a74a73a703
Likhilesh <likbalpande@example.com>
2024-10-11 02:44:36 +0530

    cors fix final try

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -14,13 +14,18 @@ import { validateIsAdmin } from "./middlewares/validateIsAdmin.js";
 const port = process.env.PORT || 5000;
 const app = express();
 
-// app.use(cors({ origin: "https://backend-abes-ticket-management-system.onrender.com", credentials: true }));
 app.use(
     cors({
-        origin: "https://abes-ticket-management-system-git-main-likbalpandes-projects.vercel.app/",
+        origin: "https://abes-ticket-management-system-git-main-likbalpandes-projects.vercel.app",
         credentials: true,
     })
 );
+// app.use(
+//     cors({
+//         origin: true,
+//         credentials: true,
+//     })
+// );
 app.use(express.json());
 app.use(cookieParser());
 
